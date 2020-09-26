0256c1ce0c3f35def1fa151e1fe2f5521b0d3bb7
rafonha <johndoe@example.comrafaela.sdp25@gmail.com>
2020-09-26 16:29:29 -0300

    working todo list

--- js/script copy.js
+++ js/script copy.js
@@ -1,9 +1,10 @@
 window.addEventListener('load', start);
 
 var globalTasks = ['Task1', 'Task2'];
-var inputTask = null;
-var currentIndex = null;
 var isEditing = false;
+var currentItem = null;
+var inputTask = null;
+var addTaskButton = document.querySelector('#addTaskButton');
 
 function start() {
     inputTask = document.querySelector('#inputTask');
@@ -19,6 +20,7 @@ function preventFormSubmit() {
     }
 
     var form = document.querySelector('form');
+
     form.addEventListener('submit', handleFormSubmit);
 }
 
@@ -32,13 +34,6 @@ function activateInput() {
     }
 
     function handleTyping(event) {
-        var hasText = !!event.target.value && event.target.value.trim() !== '';
-
-        if (!hasText) {
-            clearInput();
-            return;
-          }
-
         if (event.key === 'Enter') {
             if (isEditing) {
                 editInput(event.target.value);
@@ -46,17 +41,40 @@ function activateInput() {
                 insertTask(event.target.value);
             }
 
-            render();
             isEditing = false;
             clearInput();
         }
     }
+
+    function handleClick() {
+        var newTask = inputTask.value;
+        if (isEditing) {
+            editInput(newTask);
+        } else {
+            insertTask(newTask);
+        }
+
+        isEditing = false;
+        clearInput();
+    }
+
+    addTaskButton.addEventListener('click', handleClick);
 
     inputTask.addEventListener('keyup', handleTyping);
     inputTask.focus();
 }
 
 function render() {
+    function createSpan(task, index) {
+        var span = document.createElement('span');
+        span.classList.add('clickable');
+        span.textContent = task;
+
+        span.addEventListener('click', editTask);
+
+        return span;
+    }
+
     function createDeleteButton(index) {
         function deleteTask() {
             globalTasks.splice(index, 1);
@@ -72,20 +90,21 @@ function render() {
         return button;
     }
 
-    function createSpan(task, index) {
-        function editTask() {
-            inputTask.value = task;
-            inputTask.focus();
-            isEditing = true;
-            currentIndex = index;
-        }
+    function createEditButton(index) {
+        var button = document.createElement('button');
+        button.classList.add('editButton');
+        button.textContent =  'Edit';
 
-        var span = document.createElement('span');
-        span.classList.add('clickable');
-        span.textContent = task;
-        span.addEventListener('click', editTask);
+        button.addEventListener('click', editTask);
 
-        return span;
+        return button;
+    }
+
+    function editTask() {
+        inputTask.value = currentTask;
+        inputTask.focus();
+        isEditing = true;
+        currentIndex = index;
     }
 
     var listContainer = document.querySelector('#listContainer');
@@ -96,12 +115,14 @@ function render() {
     for (var i = 0; i < globalTasks.length; i++) {
         var currentTask = globalTasks[i];
         
-        var li = document.createElement('li');
         var deleteButton = createDeleteButton(i);
+        var editButton = createEditButton(i);
         var span = createSpan(currentTask, i);
         
+        var li = document.createElement('li');
         li.appendChild(span);
         li.appendChild(deleteButton);
+        li.appendChild(editButton);
 
         ul.appendChild(li);
     }
